4beeb6c4877ae17614e38848bbe409a2c0f8c13c
Chetna Soni <143208831+chetnasoni1310@users.noreply.github.com>
2025-05-26 12:35:15 +0530

    'update'

--- Training_Data.java
+++ Training_Data.java
@@ -7,36 +7,61 @@ import java.util.Scanner;
 public class Training_Data {
     
                           //*******************Industrial Training**************** */
-public  Map<Integer , String > IndustrialTraining(Scanner sc)
-{
-    System.out.println("Enter your Training Details : ");
-    Map<Integer , String > training = new HashMap<>();
-    for(int i=1;i<5;i++)
+
+private static final Map<Integer , String > training = new HashMap<>();
+
+
+
+
+    /**********************************************************************************************************************************/
+    public static void addTraining(Scanner sc)
     {
-        System.out.println("Have you participated in Training after "+i+" year(true/false) : ");
-        Boolean isPart = sc.nextBoolean();
+        //Taking input from user
 
-        if(isPart)
-        {
-            System.out.println("Enter your domain : ");
-            String domain = sc.nextLine();
-            training.put(i , domain);
-        }
-        else
-        {
-            training.put(i , "Not Done");
-        }
-      
-    }
-    return training;
-}            
-public static void trainingDisplay(Map<Integer, String> training) {
-    System.out.println(" Participation Summary:");
-    for (Map.Entry<Integer, String> entry : training.entrySet()) {
-        Integer year = entry.getKey();
-        String participated = entry.getValue();
-        System.out.println(year + ": " + participated);
-    }
-}
+      boolean isEntry = true;
 
+      while(isEntry) {
+      System.out.println("Enter the year : ");
+      int year = sc.nextInt();
+      sc.nextLine();
+      System.out.println("Enter the training description : ");
+      String description = sc.nextLine();
+
+
+      //Putting in the map
+      training.put(year, description);
+      System.out.println("Training added successfully !!");
+
+          System.out.println("Press 0 for stop adding or Press any key to continue : ");
+          int isStop = sc.nextInt();
+      if(isStop == 0) {
+          isEntry = false;
+      }
+      }
+    }
+
+
+
+
+    /**********************************************************************************************************************************/
+    public static void displayTraining()
+    {
+        if(training.isEmpty())
+        {
+            System.out.println("You have not done any training yet.\n" +
+                    " Please record your Industrial Training first by pressing 5 \n"
+            );
+            return;
+        }
+        System.out.println("Training Details : ");
+        for(Map.Entry<Integer , String> entry : training.entrySet())
+        {
+            System.out.println(entry.getKey()+" year : "+entry.getValue());
+        }
+    }
+
+
+
+
+/**********************************************************************************************************************************/
 }
